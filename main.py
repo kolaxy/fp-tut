625a9a71f6635de109090775ce39178ab90ead13
kolaxy <vybornoff@outlook.com>
2023-09-17 22:29:06 +0500

    query parameters

--- main.py
+++ main.py
@@ -29,9 +29,9 @@ async def get_current_user():
     return {"message": "this is the current user"}
 
 
-@app.get("/items/{item_id}")
-async def get_item(item_id: int):
-    return {"user_id": item_id}
+@app.get("/users/{user_id}")
+async def get_item(user_id: int):
+    return {"user_id": user_id}
 
 
 class FoodEnum(str, Enum):
@@ -47,3 +47,40 @@ async def get_food(food_name: FoodEnum):
     if food_name.value == "fruits":
         return {"food_name": food_name, "message": "you are still healthy, but like sweet things"}
     return {"food_name": food_name, "message": "I like chocolate milk"}
+
+fake_items_db = [{"item_name": "Foo"}, {
+    "item_name": "Bar"}, {"item_name": "Baz"}]
+
+
+@app.get("/items")
+async def list_items(skip: int = 0, limit: int = 10):
+    return fake_items_db[skip: skip + limit]
+
+
+@app.get("/items/{item_id}")
+async def get_item(item_id: str, sample_query_param: str, q: str | None = None, short: bool = False):
+    item = {"item_id": item_id, "sample_query_param": sample_query_param}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut consectetur."
+            }
+        )
+    return item
+
+@app.get("/users/{user_id}/items/{item_id}")
+async def get_user_item(
+    user_id: int, item_id: str, q: str | None = None, short: bool = False
+):
+    item = {"item_id": item_id, "ownder_id": user_id}
+    if q:
+        item.update({"q": q})
+    if not short:
+        item.update(
+            {
+                "description": "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut consectetur."
+            }
+        )
+    return item
