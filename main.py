88ca885d351d0b989f8397b1f33df746107ea372
kolaxy <vybornoff@outlook.com>
2024-01-19 16:12:20 +0300

    changes

--- main.py
+++ main.py
@@ -378,67 +378,101 @@ Part 7 -> Body - Multiple Parameters
 #     ]
 
 
-class BaseUser(BaseModel):
+# class BaseUser(BaseModel):
+#     username: str
+#     email: EmailStr
+#     full_name: str | None = None
+
+
+# class UserIn(BaseUser):
+#     password: str
+
+
+# @app.post("/user/")
+# async def create_user(user: UserIn) -> BaseUser:
+#     return user
+
+# # @app.get("/portal")
+# # async def get_portal(teleport: bool = False) -> Response:
+# #     if teleport:
+# #         return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+# #     return JSONResponse(content={"message": "Here's your interdimensional portal."})
+
+
+# @app.get("/teleport")
+# async def get_teleport() -> RedirectResponse:
+#     return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+
+
+# @app.get("/portal", response_model=None)
+# async def get_portal(teleport: bool = False) -> Response | dict:
+#     if teleport:
+#         return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
+#     return {"message": "Here's your interdimensional portal."}
+
+
+# class Item(BaseModel):
+#     name: str
+#     description: str | None = None
+#     price: float
+#     tax: float = 10.5
+
+
+# items = {
+#     "foo": {"name": "Foo", "price": 50.2},
+#     "bar": {"name": "Bar", "description": "The Bar fighters", "price": 62, "tax": 20.2},
+#     "baz": {
+#         "name": "Baz",
+#         "description": "There goes my baz",
+#         "price": 50.2,
+#         "tax": 10.5,
+#     },
+# }
+
+
+# @app.get(
+#     "/items/{item_id}/name",
+#     response_model=Item,
+#     response_model_include={"name", "description"},
+# )
+# async def read_item_name(item_id: str):
+#     return items[item_id]
+
+
+# @app.get("/items/{item_id}/public", response_model=Item, response_model_exclude={"tax"}) # OR response_model_exclude=["tax"]
+# async def read_item_public_data(item_id: str):
+#     return items[item_id]
+
+# Part 14 : Extra models
+
+class UserIn(BaseModel):
     username: str
+    passwords: str
     email: EmailStr
     full_name: str | None = None
 
 
-class UserIn(BaseUser):
-    password: str
+class UserOut(BaseModel):
+    username: str
+    email: EmailStr
+    full_name: str | None = None
 
+class UserInDB(BaseModel):
+    username: str
+    hashed_password: str
+    email: str
+    full_name: str | None = None
 
-@app.post("/user/")
-async def create_user(user: UserIn) -> BaseUser:
-    return user
+def fake_password_hasher(raw_password: str):
+    return "supersecret" + raw_password
 
-# @app.get("/portal")
-# async def get_portal(teleport: bool = False) -> Response:
-#     if teleport:
-#         return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-#     return JSONResponse(content={"message": "Here's your interdimensional portal."})
+def fake_save_user(user_in: UserIn):
+    hashed_password = fake_password_hasher(user_in.password)
+    user_in_db = UserInDB(**user_in.dict(), hashed_password=hashed_password)
+    print("User saved! ..not really")
+    return user_in_db
 
-
-@app.get("/teleport")
-async def get_teleport() -> RedirectResponse:
-    return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-
-
-@app.get("/portal", response_model=None)
-async def get_portal(teleport: bool = False) -> Response | dict:
-    if teleport:
-        return RedirectResponse(url="https://www.youtube.com/watch?v=dQw4w9WgXcQ")
-    return {"message": "Here's your interdimensional portal."}
-
-
-class Item(BaseModel):
-    name: str
-    description: str | None = None
-    price: float
-    tax: float = 10.5
-
-
-items = {
-    "foo": {"name": "Foo", "price": 50.2},
-    "bar": {"name": "Bar", "description": "The Bar fighters", "price": 62, "tax": 20.2},
-    "baz": {
-        "name": "Baz",
-        "description": "There goes my baz",
-        "price": 50.2,
-        "tax": 10.5,
-    },
-}
-
-
-@app.get(
-    "/items/{item_id}/name",
-    response_model=Item,
-    response_model_include={"name", "description"},
-)
-async def read_item_name(item_id: str):
-    return items[item_id]
-
-
-@app.get("/items/{item_id}/public", response_model=Item, response_model_exclude={"tax"}) # OR response_model_exclude=["tax"]
-async def read_item_public_data(item_id: str):
-    return items[item_id]
+@app.post("/user/", response_model=UserOut)
+async def create_user(user_in: UserIn):
+    user_saved = fake_save_user(user_in)
+    return user_saved
