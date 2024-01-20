3f7b4c36f777a0ce558bf8c53092b4cef4cc6515
kolaxy <vybornoff@outlook.com>
2024-01-20 21:30:19 +0300

    config + models optimized, crud file for test

---
+++ queries.sql
@@ -0,0 +1,39 @@
+SELECT
+    users.username,
+    users.id,
+    profiles_1.first_name,
+    profiles_1.last_name,
+    profiles_1.bio,
+    profiles_1.user_id,
+    profiles_1.id AS id_1
+FROM
+    users
+    LEFT OUTER JOIN profiles AS profiles_1 ON users.id = profiles_1.user_id
+ORDER BY
+    users.id
+SELECT
+    users.username,
+    users.id,
+    profiles_1.first_name,
+    profiles_1.last_name,
+    profiles_1.bio,
+    profiles_1.user_id,
+    profiles_1.id AS id_1
+FROM
+    users
+    LEFT OUTER JOIN profiles AS profiles_1 ON users.id = profiles_1.user_id
+ORDER BY
+    users.id
+    
+SELECT
+    users.username,
+    users.id,
+    posts_1.title,
+    posts_1.body,
+    posts_1.user_id,
+    posts_1.id AS id_1
+FROM
+    users
+    LEFT OUTER JOIN posts AS posts_1 ON users.id = posts_1.user_id
+ORDER BY
+    users.id
