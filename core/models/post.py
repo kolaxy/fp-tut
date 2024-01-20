3cb6540a1c4344e15094c2b56c5e4cf155e65a33
kolaxy <vybornoff@outlook.com>
2024-01-20 14:37:59 +0300

    created relations between user and post

--- core/models/post.py
+++ core/models/post.py
@@ -1,18 +1,18 @@
-from typing import TYPE_CHECKING
-from sqlalchemy import String, Text, ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column, relationship
+from sqlalchemy import String, Text
+from sqlalchemy.orm import Mapped, mapped_column
 
 from .base import Base
-
-if TYPE_CHECKING:
-    from .user import User
+from .mixins import UserRelationMixin
 
 
-class Post(Base):
-    title: Mapped[str] = mapped_column(String(100), unique=False)
-    body: Mapped[str] = mapped_column(Text, default="", server_default="")
-    user_id: Mapped[int] = mapped_column(
-        ForeignKey("users.id"),
-        # nullable=False,
+class Post(UserRelationMixin, Base):
+    _user_back_populates = "posts"
+    title: Mapped[str] = mapped_column(
+        String(100),
+        unique=False,
     )
-    user: Mapped["User"] = relationship(back_populates="posts")
+    body: Mapped[str] = mapped_column(
+        Text,
+        default="",
+        server_default="",
+    )
